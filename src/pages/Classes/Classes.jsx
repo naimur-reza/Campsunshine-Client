c12dce253e3583f0669e50977638f223ae024c03
Naimur Reza <naimur.rezaa@gmail.com>
2023-06-09 01:30:52 +0600

    created get class api installed tanstack query

--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import SectionTitle from "../../components/shared/SectionTitle/SectionTitle";
+import { getClasses } from "../../api/classes";
+import { set } from "react-hook-form";
 
 const Classes = () => {
+  const [classes, setClasses] = useState([]);
+
   return (
-    <div className="pt-[89px]">
+    <div className="pt-[92px]">
       <SectionTitle title="All Classes" subTitle="Find what you want!" />
     </div>
   );

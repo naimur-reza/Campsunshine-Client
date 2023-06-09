c7c0b3f84a02e50b61d5b109f59c9b4d98572eff
Naimur Reza <naimur.rezaa@gmail.com>
2023-06-09 23:56:38 +0600

    done with classes in approved classes added new

--- src/pages/Classes/Classes.jsx
+++ src/pages/Classes/Classes.jsx
@@ -1,19 +1,31 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import SectionTitle from "../../components/shared/SectionTitle/SectionTitle";
 import { AuthContext } from "../../providers/AuthProvider";
 import { getAllClasses } from "../../api/classes";
+import ClassesCard from "../../components/Home/Classes/ClassesCard";
 
 const Classes = () => {
-  // const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
   const { user } = useContext(AuthContext);
+  const [classes, setClasses] = useState([]);
+  useEffect(() => {
+    setLoading(true);
+    getAllClasses().then((res) => {
+      setClasses(res);
+      setLoading(false);
+    });
+  }, [user]);
 
-  const classes = getAllClasses().then((data) => {
-    console.log(data);
-    // setLoading(false);
-  });
   return (
-    <div className="pt-[92px]">
+    <div className="pt-[92px] my-container">
       <SectionTitle title="All Classes" subTitle="Find what you want!" />
+
+      <div className="grid grid-cols-2 lg:grid-cols-3 place-items-center gap-5">
+        {classes &&
+          classes.map((classInfo) => {
+            return <ClassesCard key={classInfo._id} classInfo={classInfo} />;
+          })}
+      </div>
     </div>
   );
 };

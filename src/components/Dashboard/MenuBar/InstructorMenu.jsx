4cbaf5c2e500fb2da4ff5dce01291a0247486810
Naimur Reza <naimur.rezaa@gmail.com>
2023-06-10 23:57:43 +0600

    teacher menu updated

--- src/components/Dashboard/MenuBar/InstructorMenu.jsx
+++ src/components/Dashboard/MenuBar/InstructorMenu.jsx
@@ -7,16 +7,6 @@ const InstructorMenu = () => {
     <div className="uppercase space-y-4 ">
       <NavLink
         className={({ isActive }) =>
-          `flex items-center px-4 py-2 mt-5  gap-3 transition-colors duration-200 transform  hover:bg-teal-400   hover:text-white ${
-            isActive ? "  bg-teal-400 text-white" : "text-gray-600"
-          }`
-        }
-        to={"/dashboard/add-class"}>
-        <SiGoogleclassroom size={20} />
-        Add A Class
-      </NavLink>
-      <NavLink
-        className={({ isActive }) =>
           `flex items-center px-4 py-2 mt-5 gap-3  transition-colors duration-200 transform  hover:bg-teal-400   hover:text-white ${
             isActive ? " bg-teal-400 text-white" : "text-gray-600"
           } `
@@ -24,6 +14,16 @@ const InstructorMenu = () => {
         to={"/dashboard/my-classes"}>
         <FiUsers size={20} />
         My classes
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          `flex items-center px-4 py-2 mt-5  gap-3 transition-colors duration-200 transform  hover:bg-teal-400   hover:text-white ${
+            isActive ? "  bg-teal-400 text-white" : "text-gray-600"
+          }`
+        }
+        to={"/dashboard/add-class"}>
+        <SiGoogleclassroom size={20} />
+        Add A Class
       </NavLink>
     </div>
   );

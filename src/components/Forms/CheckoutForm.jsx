fe1fc701078b7dd61d67e390bb953a76dba59623
Naimur Reza <naimur.rezaa@gmail.com>
2023-06-11 15:19:38 +0600

    done with payment integrations

--- src/components/Forms/CheckoutForm.jsx
+++ src/components/Forms/CheckoutForm.jsx
@@ -4,14 +4,19 @@ import "./CheckoutStyle.css";
 import { AuthContext } from "../../providers/AuthProvider";
 import axios from "axios";
 import { toast } from "react-hot-toast";
+import { ImSpinner3 } from "react-icons/im";
+import Spinner3 from "../shared/Spinner/Spinner3";
 const CheckoutForm = ({ closeModal, classInfo }) => {
   const [cardError, setCardError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [buttonLoading, setButtonLoading] = useState(false);
   const [clientSecret, setClientSecret] = useState(null);
   const stripe = useStripe();
   const elements = useElements();
   const { user } = useContext(AuthContext);
 
   const handleSubmit = async (event) => {
+    setButtonLoading(true);
     // Block native form submission.
     event.preventDefault();
 
@@ -61,15 +66,17 @@ const CheckoutForm = ({ closeModal, classInfo }) => {
     } else {
       console.log("[paymentIntent]", paymentIntent);
       if (paymentIntent.status === "succeeded") {
+        // todo : delete from selected by api
         const paymentInfo = {
           ...classInfo,
           paymentIntentId: paymentIntent.id,
         };
         closeModal();
+
         toast.success(
           <p className="text-sm">
             Payment Successful. Id:
-            <span className="bg-teal-500 text-xs mx-3 text-white p-1 rounded-full">
+            <span className="bg-gray-200 text-xs mx-3  p-1 rounded-full">
               {paymentIntent.id}
             </span>
             🤑
@@ -79,6 +86,7 @@ const CheckoutForm = ({ closeModal, classInfo }) => {
     }
   };
   useEffect(() => {
+    setLoading(true);
     // todo : useAxios secure
     if (classInfo.price) {
       axios
@@ -89,14 +97,24 @@ const CheckoutForm = ({ closeModal, classInfo }) => {
         .then((res) => {
           console.log(res.data.clientSecret);
           setClientSecret(res.data.clientSecret);
+          setLoading(false);
         })
         .catch((err) => {
           console.log(err);
+          setLoading(false);
         });
     }
   }, [classInfo]);
+
+  if (loading) {
+    return <Spinner3 />;
+  }
   return (
     <>
+      <h1 className="text-lg font-semibold text-gray-700 mb-4">Payment</h1>
+      <img src={classInfo.image} className="w-full rounded-lg mb-5" alt="" />
+      <h1 className="text-gray-600 mb-2">{classInfo.className}</h1>
+      <p className="text-gray-600 ">Price: ${classInfo.price}</p>
       <form onSubmit={handleSubmit}>
         <CardElement
           options={{
@@ -116,13 +134,17 @@ const CheckoutForm = ({ closeModal, classInfo }) => {
         />
         <div className="mt-4">
           <button
-            disabled={!stripe}
+            disabled={!stripe || loading}
             type="submit
           "
             className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
             // onClick={closeModal}>
           >
-            Got it, thanks!
+            {buttonLoading ? (
+              <ImSpinner3 size={22} className="animate-spin" />
+            ) : (
+              "Pay"
+            )}
           </button>
         </div>
       </form>
---
+++ src/components/shared/Spinner/Spinner3.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Spinner3 = () => {
+  return (
+    <div className="h-full flex items-center justify-center">
+      <div className="w-12 h-12 border-[5px] border-red-500 rounded-full border-dotted animate-spin" />
+    </div>
+  );
+};
+
+export default Spinner3;

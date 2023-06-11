55b05e0b6fa06698630893b90d0908690fb79fbb
Naimur Reza <naimur.rezaa@gmail.com>
2023-06-11 21:48:08 +0600

    added popular instructors

--- src/components/Home/PopularInstructors/InstructorCard.jsx
+++ src/components/Home/PopularInstructors/InstructorCard.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import { BsFacebook, BsInstagram, BsTwitter } from "react-icons/bs";
 
 const InstructorCard = ({ info }) => {
-  const { name, teaching, students, email, image } = info;
+  const { teacher, className, email, image, enrolled } = info;
   return (
     <>
       <div className=" bg-black/20  overflow-hidden backdrop-blur-sm rounded  relative p-4 space-y-2 text-center ">
-        <img src={image} alt="" />
+        <img src={teacher.image} className="h-64 object-cover w-full" alt="" />
         <h1 className="text-lg font-semibold text-yellow-400">{name}</h1>
-        <h2 className="text-sm text-gray-100">{teaching}</h2>
-        <p className="text-sm text-gray-100">Students: {students} </p>
-        <p className="bg-gray-200 text-sm py-1 rounded text-gray-700">
+        <h2 className="text-sm text-rose-200">{className}</h2>
+        <p className="text-sm text-gray-100">Students: {enrolled} </p>
+        <p className="bg-white/80  text-sm py-1 rounded text-gray-700">
           {email}
         </p>
         <div className="absolute inset-0   flex items-center justify-center bg-black/40 opacity-0 hover:opacity-100 duration-400 cursor-pointer transition ">
           <div>
-            <div className="flex gap-5  ">
+            <div className="flex gap-5  justify-center ">
               <BsFacebook
                 size={24}
                 className="text-sky-400 hover:text-sky-500 transition-all  cursor-pointer"
@@ -29,7 +29,7 @@ const InstructorCard = ({ info }) => {
                 className="text-sky-500  hover:text-sky-600 transition-all cursor-pointer"
               />
             </div>
-            <h1 className="mt-8 text-gray-100">{teaching}</h1>
+            <h1 className="mt-8 text-gray-100 px-5">{className}</h1>
           </div>
         </div>
       </div>
--- src/components/Home/PopularInstructors/PopularInstructors.jsx
+++ src/components/Home/PopularInstructors/PopularInstructors.jsx
@@ -1,6 +1,6 @@
 import SectionTitle from "../../shared/SectionTitle/SectionTitle";
 import InstructorCard from "./InstructorCard";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -13,65 +13,75 @@ import "./style.css";
 
 // import required modules
 import { FreeMode, Pagination } from "swiper";
+import { getPopularClasses } from "../../../api/classes";
 const PopularInstructors = () => {
   // todo: add popular instructors data from api
+  const [teachers, setTeachers] = useState(null);
 
-  const teachers = [
-    {
-      _id: 1,
-      name: "John Doe",
-      teaching: "Bangla",
-      students: 100,
-      email: "jhon@gmail.com",
-      image: "https://themesfamily.com/tm/hadi/assets/img/team/1.jpg",
-    },
-    {
-      _id: 2,
-      name: "John Doe",
-      teaching: "English",
-      students: 100,
-      email: "jhon@gmail.com",
-      image: "https://themesfamily.com/tm/hadi/assets/img/team/1.jpg",
-    },
-    {
-      _id: 3,
-      name: "John Doe",
-      teaching: "Math",
-      students: 100,
-      email: "jhon@gmail.com",
-      image: "https://themesfamily.com/tm/hadi/assets/img/team/1.jpg",
-    },
-    {
-      _id: 4,
-      name: "John Doe",
-      teaching: "Biology",
-      students: 100,
-      email: "jhon@gmail.com",
-      image: "https://themesfamily.com/tm/hadi/assets/img/team/1.jpg",
-    },
-    {
-      _id: 5,
-      name: "John Doe",
-      teaching: "Bangla",
-      students: 100,
-      email: "jhon@gmail.com",
-      image: "https://themesfamily.com/tm/hadi/assets/img/team/1.jpg",
-    },
-    {
-      _id: 6,
-      name: "John Doe",
-      teaching: "Physics",
-      students: 100,
-      email: "jhon@gmail.com",
-      image: "https://themesfamily.com/tm/hadi/assets/img/team/1.jpg",
-    },
-  ];
+  // const teachers = [
+  //   {
+  //     _id: 1,
+  //     name: "John Doe",
+  //     teaching: "Bangla",
+  //     students: 100,
+  //     email: "jhon@gmail.com",
+  //     image: "https://themesfamily.com/tm/hadi/assets/img/team/1.jpg",
+  //   },
+  //   {
+  //     _id: 2,
+  //     name: "John Doe",
+  //     teaching: "English",
+  //     students: 100,
+  //     email: "jhon@gmail.com",
+  //     image: "https://themesfamily.com/tm/hadi/assets/img/team/1.jpg",
+  //   },
+  //   {
+  //     _id: 3,
+  //     name: "John Doe",
+  //     teaching: "Math",
+  //     students: 100,
+  //     email: "jhon@gmail.com",
+  //     image: "https://themesfamily.com/tm/hadi/assets/img/team/1.jpg",
+  //   },
+  //   {
+  //     _id: 4,
+  //     name: "John Doe",
+  //     teaching: "Biology",
+  //     students: 100,
+  //     email: "jhon@gmail.com",
+  //     image: "https://themesfamily.com/tm/hadi/assets/img/team/1.jpg",
+  //   },
+  //   {
+  //     _id: 5,
+  //     name: "John Doe",
+  //     teaching: "Bangla",
+  //     students: 100,
+  //     email: "jhon@gmail.com",
+  //     image: "https://themesfamily.com/tm/hadi/assets/img/team/1.jpg",
+  //   },
+  //   {
+  //     _id: 6,
+  //     name: "John Doe",
+  //     teaching: "Physics",
+  //     students: 100,
+  //     email: "jhon@gmail.com",
+  //     image: "https://themesfamily.com/tm/hadi/assets/img/team/1.jpg",
+  //   },
+  // ];
+
+  useEffect(() => {
+    getPopularClasses().then((res) => {
+      console.log(res);
+      setTeachers(res);
+    });
+  }, []);
+
   return (
     <div className="blob py-14">
       <SectionTitle
         color={"text-gray-200"}
         title="Popular Instructors"
-        subTitle="Our best teachers"
+        subTitle="Meet Popular Instructors based on student "
       />
 
       <Swiper

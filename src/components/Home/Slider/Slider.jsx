fea7f1be4ae4635a7b97c3a413388ead4c2b4085
Naimur Reza <naimur.rezaa@gmail.com>
2023-06-12 13:24:19 +0600

    added slider and text also modified colors

--- src/components/Home/Slider/Slider.jsx
+++ src/components/Home/Slider/Slider.jsx
@@ -9,11 +9,31 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { EffectFade, Navigation, Pagination } from "swiper";
 
 const BannerSlider = () => {
-  const images = [
-    " https://i.ibb.co/XZMxsHd/national-cancer-institute-N-aihp118p8-unsplash.jpg",
-    "https://i.ibb.co/428JpVY/cdc-GDok-EYn-Ofn-E-unsplash.jpg",
-    "https://i.ibb.co/9gHfGY7/kenny-eliason-z-FSo6bn-ZJTw-unsplash.jpg",
+  const data = [
+    {
+      image:
+        "https://i.ibb.co/XZMxsHd/national-cancer-institute-N-aihp118p8-unsplash.jpg",
+      title: "Fun-Filled Adventures Await",
+      description:
+        "Discover an exciting summer experience filled with outdoor activities, educational programs, and lifelong memories. Enroll your child today.",
+    },
+    {
+      image: "https://i.ibb.co/428JpVY/cdc-GDok-EYn-Ofn-E-unsplash.jpg",
+      title: "Discover the Joy of Learning Outdoors",
+      description:
+        "Step into nature for hands-on exploration, teamwork, and innovative lessons. Give your child a summer of education and fun.",
+    },
+    {
+      image:
+        "https://i.ibb.co/9gHfGY7/kenny-eliason-z-FSo6bn-ZJTw-unsplash.jpg",
+      title: "Unlock Your Child's Potential",
+      description:
+        "Nurture your child's talents and passions in a supportive environment. Join us for a summer of growth and achievement.",
+    },
   ];
+  // " https://i.ibb.co/XZMxsHd/national-cancer-institute-N-aihp118p8-unsplash.jpg",
+  // "https://i.ibb.co/428JpVY/cdc-GDok-EYn-Ofn-E-unsplash.jpg",
+  // "https://i.ibb.co/9gHfGY7/kenny-eliason-z-FSo6bn-ZJTw-unsplash.jpg",
 
   return (
     <>
@@ -26,13 +46,23 @@ const BannerSlider = () => {
         }}
         modules={[EffectFade, Navigation, Pagination]}
         className="mySwiper">
-        {images.map((image, index) => (
+        {data.map((item, index) => (
           <SwiperSlide key={index}>
             <img
-              src={image}
+              src={item.image}
               alt="banner"
               className="h-[100vh] object-cover w-full"
             />
+            <div className="absolute inset-0 z-10 flex items-center px-10 bg-black/40 dark:bg-black/60">
+              <div className="lg:w-1/2 ">
+                <h1 className="lg:text-5xl text-3xl font-bold text-white">
+                  {item.title}
+                </h1>
+                <p className="text-gray-200 pt-6 lg:text-lg text-base tracking-wide  ">
+                  {item.description}
+                </p>
+              </div>
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
